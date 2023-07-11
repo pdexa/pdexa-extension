71259cb92eedeed7f5a4b793caae9dffb68b30f9
Marcel Koch <marcel.koch@kit.edu>
2023-07-11 10:47:33 +0200

    only allow deal.ii without ginkgo

--- third-party/deal.ii.cmake
+++ third-party/deal.ii.cmake
@@ -2,4 +2,10 @@ message(STATUS "Enabling package deal.II")
 
 find_package(deal.II REQUIRED)
 
-DEAL_II_INITIALIZE_CACHED_VARIABLES()
+deal_ii_initialize_cached_variables()
+
+if (DEAL_II_WITH_GINKGO)
+    message(FATAL_ERROR
+            "The deal.II installation has Ginkgo enabled. Please disable it to prevent conflicts with the "
+            "wrappers implemented in PDExa.")
+endif ()
