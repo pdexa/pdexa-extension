8a84bbad3e68c55049a4ee92892f22ddf1c0cd3a
Marcel Koch <marcel.koch@kit.edu>
2024-10-30 10:46:21 +0100

    make libceed findable through pkg-config

--- third-party/libceed.cmake
+++ third-party/libceed.cmake
@@ -1,5 +1,22 @@
 message(STATUS "Enabling package libCEED")
 
-find_library(libceed libceed HINTS ${libCEED_DIR})
-message("Found libCEED at ${libCEED}") 
+include(FindPkgConfig)
+pkg_search_module(
+        libCEED
+        QUIET
+        IMPORTED_TARGET
+        ceed
+)
+
+if (libCEED_FOUND)
+    message(STATUS "Found libCEED at ${libCEED_LIBRARY_DIRS}")
+else ()
+    message(STATUS "Try finding libCEED manually")
+    find_library(libceed libceed HINTS ${libCEED_DIR})
+    if (libCEED)
+        message(STATUS "Found libCEED at ${libCEED}")
+    else ()
+        message(FATAL_ERROR "Could not locate libCEED")
+    endif ()
+endif ()
 
