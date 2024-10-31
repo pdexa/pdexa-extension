25fdb95e22fd13b80829630273ad449f09cdea0a
Marcel Koch <marcel.koch@kit.edu>
2024-10-31 13:48:53 +0000

    [wip] formatting

--- app/step-batch-solver/batch-solver.cpp
+++ app/step-batch-solver/batch-solver.cpp
@@ -33,8 +33,7 @@ struct custom_operator_item {
   gko::int32 num_cols;
 };
 
-constexpr custom_operator_item extract_batch_item(custom_operator_view op,
-                                                  gko::size_type batch_id) {
+constexpr custom_operator_item extract_batch_item(custom_operator_view op, gko::size_type batch_id) {
   return {op.num_batch_items, batch_id, op.num_rows, op.num_cols};
 }
 
@@ -44,9 +43,8 @@ public:
 
   struct const_item {};
 
-  explicit CustomOperator(std::shared_ptr<const gko::Executor> exec,
-                          gko::batch_dim<2> size = {}) :
-    EnablePolymorphicObject(std::move(exec)), size_(size) {}
+  explicit CustomOperator(std::shared_ptr<const gko::Executor> exec, gko::batch_dim<2> size = {}) :
+      EnablePolymorphicObject(std::move(exec)), size_(size) {}
 
   [[nodiscard]] gko::batch_dim<2> get_size() const { return size_; }
 
@@ -60,76 +58,71 @@ private:
 
 // TODO: make this either a class member or use CPO
 [[nodiscard]] dummy::custom_operator_view create_view(const dummy::CustomOperator* x) {
-  return {
-    x->get_num_batch_items(),
-    static_cast<gko::int32>(x->get_common_size()[0]),
-    static_cast<gko::int32>(x->get_common_size()[1])
-  };
+  return {x->get_num_batch_items(),
+          static_cast<gko::int32>(x->get_common_size()[0]),
+          static_cast<gko::int32>(x->get_common_size()[1])};
 }
 
-constexpr void advanced_apply(
-  double alpha,
-  custom_operator_item a,
-  gko::batch::multi_vector::batch_item<const double> b,
-  double beta,
-  gko::batch::multi_vector::batch_item<double> x,
-  [[maybe_unused]] std::variant<gko::reference_kernel, gko::omp_kernel>) {
+constexpr void advanced_apply(double alpha,
+                              custom_operator_item a,
+                              gko::batch::multi_vector::batch_item<const double> b,
+                              double beta,
+                              gko::batch::multi_vector::batch_item<double> x,
+                              [[maybe_unused]] std::variant<gko::reference_kernel, gko::omp_kernel>) {
   auto num_batches = a.num_batches;
   for (gko::size_type row = 0; row < static_cast<gko::size_type>(a.num_rows); ++row) {
     double acc{};
 
-    if (row > 0) { acc += -gko::one<double>() * b.values[row - 1]; }
-    acc += (2.0 + static_cast<double>(a.batch_id) / static_cast<double>(num_batches)) *
-      b.values[row];
-    if (row < static_cast<gko::size_type>(a.num_rows - 1)) { acc += -gko::one<double>() * b.values[row + 1]; }
+    if (row > 0) {
+      acc += -gko::one<double>() * b.values[row - 1];
+    }
+    acc += (2.0 + static_cast<double>(a.batch_id) / static_cast<double>(num_batches)) * b.values[row];
+    if (row < static_cast<gko::size_type>(a.num_rows - 1)) {
+      acc += -gko::one<double>() * b.values[row + 1];
+    }
     x.values[row] = alpha * acc + beta * x.values[row];
   }
 }
 
-constexpr void simple_apply(
-  const custom_operator_item& a,
-  const gko::batch::multi_vector::batch_item<const double>& b,
-  const gko::batch::multi_vector::batch_item<double>& x,
-  [[maybe_unused]] std::variant<gko::reference_kernel, gko::omp_kernel>) {
+constexpr void simple_apply(const custom_operator_item& a,
+                            const gko::batch::multi_vector::batch_item<const double>& b,
+                            const gko::batch::multi_vector::batch_item<double>& x,
+                            [[maybe_unused]] std::variant<gko::reference_kernel, gko::omp_kernel>) {
   advanced_apply(1.0, a, b, 0.0, x, gko::reference_kernel{});
 }
 
 
 #if defined(GKO_COMPILING_CUDA) || defined(GKO_COMPILING_HIP)
 
-__device__ void advanced_apply(
-    double alpha, custom_operator_item a,
-    gko::batch::multi_vector::batch_item<const double> b, double beta,
-    gko::batch::multi_vector::batch_item<double> x,
-    [[maybe_unused]] gko::cuda_hip_kernel)
-{
-    auto tidx = threadIdx.x;
-    auto num_batches = a.num_batches;
-    for (gko::size_type row = tidx; row < a.num_rows; row += blockDim.x) {
-        double acc{};
+__device__ void advanced_apply(double alpha,
+                               custom_operator_item a,
+                               gko::batch::multi_vector::batch_item<const double> b,
+                               double beta,
+                               gko::batch::multi_vector::batch_item<double> x,
+                               [[maybe_unused]] gko::cuda_hip_kernel) {
+  auto tidx = threadIdx.x;
+  auto num_batches = a.num_batches;
+  for (gko::size_type row = tidx; row < a.num_rows; row += blockDim.x) {
+    double acc{};
 
-        if (row > 0) {
-            acc += -gko::one<double>() * b.values[row - 1];
-        }
-        acc +=
-            (static_cast<double>(2.0) + static_cast<double>(a.batch_id) /
-                                            static_cast<double>(num_batches)) *
-            b.values[row];
-        if (row < a.num_rows - 1) {
-            acc += -gko::one<double>() * b.values[row + 1];
-        }
-        // auto dummy = alpha * acc + beta * x[row];
-        x.values[row] = alpha * acc + beta * x.values[row];
+    if (row > 0) {
+      acc += -gko::one<double>() * b.values[row - 1];
     }
+    acc +=
+      (static_cast<double>(2.0) + static_cast<double>(a.batch_id) / static_cast<double>(num_batches)) * b.values[row];
+    if (row < a.num_rows - 1) {
+      acc += -gko::one<double>() * b.values[row + 1];
+    }
+    // auto dummy = alpha * acc + beta * x[row];
+    x.values[row] = alpha * acc + beta * x.values[row];
+  }
 }
 
-__device__ void simple_apply(
-    const custom_operator_item& a,
-    const gko::batch::multi_vector::batch_item<const double>& b,
-    const gko::batch::multi_vector::batch_item<double>& x,
-    [[maybe_unused]] gko::cuda_hip_kernel tag)
-{
-    advanced_apply(1.0, a, b, 0.0, x, tag);
+__device__ void simple_apply(const custom_operator_item& a,
+                             const gko::batch::multi_vector::batch_item<const double>& b,
+                             const gko::batch::multi_vector::batch_item<double>& x,
+                             [[maybe_unused]] gko::cuda_hip_kernel tag) {
+  advanced_apply(1.0, a, b, 0.0, x, tag);
 }
 
 #endif
@@ -155,11 +148,9 @@ using cg_op = gko::batch_template::solver::Cg<op_type>;
 namespace detail {
 template<typename InputType>
 auto unbatch(const InputType* batch_object) {
-  auto unbatched_mats =
-    std::vector<std::unique_ptr<typename InputType::unbatch_type>>{};
+  auto unbatched_mats = std::vector<std::unique_ptr<typename InputType::unbatch_type>>{};
   for (size_type b = 0; b < batch_object->get_num_batch_items(); ++b) {
-    unbatched_mats.emplace_back(
-      batch_object->create_const_view_for_item(b)->clone());
+    unbatched_mats.emplace_back(batch_object->create_const_view_for_item(b)->clone());
   }
   return unbatched_mats;
 }
@@ -170,28 +161,17 @@ int main(int argc, char* argv[]) {
   // Print ginkgo version information
   std::cout << gko::version_info::get() << std::endl;
 
-  cxxopts::Options options(
-    "batched-matrix-free",
-    "Solve a batched problem with the external matrix format.");
+  cxxopts::Options options("batched-matrix-free", "Solve a batched problem with the external matrix format.");
 
   options.add_options()(
-    "executor",
-    "The Ginkgo Executor type",
-    cxxopts::value<std::string>()->default_value("reference"))(
-    "b,batches",
-    "The number of batches",
-    cxxopts::value<value_type>()->default_value("2"))(
-    "s,size",
-    "The (square) size of a batch item",
-    cxxopts::value<size_type>()->default_value("32"))(
+    "executor", "The Ginkgo Executor type", cxxopts::value<std::string>()->default_value("reference"))(
+    "b,batches", "The number of batches", cxxopts::value<value_type>()->default_value("2"))(
+    "s,size", "The (square) size of a batch item", cxxopts::value<size_type>()->default_value("32"))(
     "print-residuals",
     "Print the final residuals",
     cxxopts::value<bool>()->default_value("false")->implicit_value("true"))(
-    "matrix-free",
-    "Use external matrix format",
-    cxxopts::value<bool>()->default_value("true")->implicit_value("true"))(
-    "h,help",
-    "Show this message");
+    "matrix-free", "Use external matrix format", cxxopts::value<bool>()->default_value("true")->implicit_value("true"))(
+    "h,help", "Show this message");
   options.parse_positional("executor");
   options.allow_unrecognised_options();
   auto args = options.parse(argc, argv);
@@ -213,35 +193,12 @@ int main(int argc, char* argv[]) {
   // executor and all the other functions/ routines within Ginkgo should
   // automatically work and run on the executor with any other changes.
   const auto executor_string = args["executor"].as<std::string>();
-  std::map<std::string, std::function<std::shared_ptr<gko::Executor>()>>
-    exec_map{
-      {"omp", [] { return gko::OmpExecutor::create(); }},
-      {
-        "cuda",
-        [] {
-          return gko::CudaExecutor::create(
-            0,
-            gko::ReferenceExecutor::create());
-        }
-      },
-      {
-        "hip",
-        [] {
-          return gko::HipExecutor::create(
-            0,
-            gko::ReferenceExecutor::create());
-        }
-      },
-      {
-        "dpcpp",
-        [] {
-          return gko::DpcppExecutor::create(
-            0,
-            gko::ReferenceExecutor::create());
-        }
-      },
-      {"reference", [] { return gko::ReferenceExecutor::create(); }}
-    };
+  std::map<std::string, std::function<std::shared_ptr<gko::Executor>()>> exec_map{
+    {"omp", [] { return gko::OmpExecutor::create(); }},
+    {"cuda", [] { return gko::CudaExecutor::create(0, gko::ReferenceExecutor::create()); }},
+    {"hip", [] { return gko::HipExecutor::create(0, gko::ReferenceExecutor::create()); }},
+    {"dpcpp", [] { return gko::DpcppExecutor::create(0, gko::ReferenceExecutor::create()); }},
+    {"reference", [] { return gko::ReferenceExecutor::create(); }}};
 
   // executor where Ginkgo will perform the computation
   const auto exec = exec_map.at(executor_string)(); // throws if not valid
@@ -256,14 +213,11 @@ int main(int argc, char* argv[]) {
 
   // @sect3{Generate data}
   // Create batch_dim object to describe the dimensions of the batch matrix.
-  auto batch_mat_size =
-    gko::batch_dim<2>(num_systems, gko::dim<2>(num_rows, num_rows));
-  auto batch_vec_size =
-    gko::batch_dim<2>(num_systems, gko::dim<2>(num_rows, 1));
+  auto batch_mat_size = gko::batch_dim<2>(num_systems, gko::dim<2>(num_rows, num_rows));
+  auto batch_vec_size = gko::batch_dim<2>(num_systems, gko::dim<2>(num_rows, 1));
   auto nnz = 3 * (num_rows - 2) + 4;
 
-  auto A_mtx =
-    gko::share(mtx_type::create(exec->get_master(), batch_mat_size, nnz));
+  auto A_mtx = gko::share(mtx_type::create(exec->get_master(), batch_mat_size, nnz));
   auto A_op = std::make_shared<op_type>(exec, batch_mat_size);
 
   if (args["print-residuals"].as<bool>() || !args["matrix-free"].as<bool>()) {
@@ -292,7 +246,7 @@ int main(int argc, char* argv[]) {
       vals[nnz - 2] = -1.0;
       vals[nnz - 1] = diag_val;
     };
-#pragma omp parallel for firstprivate(md)
+#pragma omp parallel for
     for (gko::size_type id = 0; id < batch_mat_size.get_num_batch_items();
          ++id) {
       fill_values(id, A_mtx->get_values_for_item(id));
@@ -310,36 +264,30 @@ int main(int argc, char* argv[]) {
   // @sect3{Create the batch solver factory}
   const real_type reduction_factor{1e-10};
   // Create a batched solver factory with relevant parameters.
-  using SolverVariant =
-    std::variant<std::shared_ptr<cg>, std::shared_ptr<cg_op>>;
-  auto solver{
-    args["matrix-free"].as<bool>()
-      ? SolverVariant(cg_op::build()
-                      .with_max_iterations(500)
-                      .with_tolerance(reduction_factor)
-                      .with_tolerance_type(
-                        gko::batch::stop::tolerance_type::relative)
-                      .on(exec)
-                      ->generate(A_op))
-      : SolverVariant(cg::build()
-                      .with_max_iterations(500)
-                      .with_tolerance(reduction_factor)
-                      .with_tolerance_type(
-                        gko::batch::stop::tolerance_type::relative)
-                      .on(exec)
-                      ->generate(A_mtx))
-  };
+  using SolverVariant = std::variant<std::shared_ptr<cg>, std::shared_ptr<cg_op>>;
+  auto solver{args["matrix-free"].as<bool>()
+                ? SolverVariant(cg_op::build()
+                                  .with_max_iterations(500)
+                                  .with_tolerance(reduction_factor)
+                                  .with_tolerance_type(gko::batch::stop::tolerance_type::relative)
+                                  .on(exec)
+                                  ->generate(A_op))
+                : SolverVariant(cg::build()
+                                  .with_max_iterations(500)
+                                  .with_tolerance(reduction_factor)
+                                  .with_tolerance_type(gko::batch::stop::tolerance_type::relative)
+                                  .on(exec)
+                                  ->generate(A_mtx))};
 
   // @sect3{Batch logger}
   // Create a logger to obtain the iteration counts and "implicit" residual
   //  norms for every system after the solve.
-  std::shared_ptr<const gko::batch::log::BatchConvergence<value_type>>
-    logger = gko::batch::log::BatchConvergence<value_type>::create();
+  std::shared_ptr<const gko::batch::log::BatchConvergence<value_type>> logger =
+    gko::batch::log::BatchConvergence<value_type>::create();
 
   // @sect3{Generate and solve}
   // add the logger to the solver
-  std::visit([logger](auto& solver_v) { solver_v->add_logger(logger); },
-             solver);
+  std::visit([logger](auto& solver_v) { solver_v->add_logger(logger); }, solver);
 
 
   exec->synchronize();
@@ -349,14 +297,12 @@ int main(int argc, char* argv[]) {
 
   exec->synchronize();
   std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
-  auto time_span =
-    std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+  auto time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
 
   std::cout << "Solver type: "
-    << "batch::cg"
-    << "\nMatrix size: " << A_mtx->get_common_size()
-    << "\nNum batch entries: " << A_mtx->get_num_batch_items()
-    << "\nTime elapsed: " << time_span.count() << std::endl;
+            << "batch::cg"
+            << "\nMatrix size: " << A_mtx->get_common_size() << "\nNum batch entries: " << A_mtx->get_num_batch_items()
+            << "\nTime elapsed: " << time_span.count() << std::endl;
 
   if (args["print-residuals"].as<bool>()) {
     // @sect3{Check result}
@@ -377,16 +323,11 @@ int main(int argc, char* argv[]) {
     // @todo: change this when the external apply becomes available
     A_mtx->apply(one, x, neg_one, res);
     // allocate and compute residual norm
-    auto host_res_norm =
-      real_vec_type::create(exec->get_master(), norm_dim);
+    auto host_res_norm = real_vec_type::create(exec->get_master(), norm_dim);
     host_res_norm->fill(0.0);
     res->compute_norm2(host_res_norm);
-    auto host_log_resid = gko::make_temporary_clone(
-      exec->get_master(),
-      &logger->get_residual_norm());
-    auto host_log_iters = gko::make_temporary_clone(
-      exec->get_master(),
-      &logger->get_num_iterations());
+    auto host_log_resid = gko::make_temporary_clone(exec->get_master(), &logger->get_residual_norm());
+    auto host_log_iters = gko::make_temporary_clone(exec->get_master(), &logger->get_num_iterations());
 
     std::cout << "Residual norm sqrt(r^T r):\n";
     // "unbatch" converts a batch object into a vector of objects of the
@@ -395,17 +336,12 @@ int main(int argc, char* argv[]) {
     auto unb_res = detail::unbatch(host_res_norm.get());
     auto unb_bnorm = detail::unbatch(host_b_norm.get());
     for (size_type i = 0; i < num_systems; ++i) {
-      std::cout << " System no. " << i
-        << ": residual norm = " << unb_res[i]->at(0, 0)
-        << ", implicit residual norm = "
-        << host_log_resid->get_const_data()[i]
-        << ", iterations = "
-        << host_log_iters->get_const_data()[i] << std::endl;
-      const real_type relresnorm =
-        unb_res[i]->at(0, 0) / unb_bnorm[i]->at(0, 0);
+      std::cout << " System no. " << i << ": residual norm = " << unb_res[i]->at(0, 0)
+                << ", implicit residual norm = " << host_log_resid->get_const_data()[i]
+                << ", iterations = " << host_log_iters->get_const_data()[i] << std::endl;
+      const real_type relresnorm = unb_res[i]->at(0, 0) / unb_bnorm[i]->at(0, 0);
       if (!(relresnorm <= reduction_factor)) {
-        std::cout << "System " << i << " converged only to "
-          << relresnorm << " relative residual." << std::endl;
+        std::cout << "System " << i << " converged only to " << relresnorm << " relative residual." << std::endl;
       }
     }
   }
