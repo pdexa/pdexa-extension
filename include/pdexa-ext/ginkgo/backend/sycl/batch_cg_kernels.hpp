ef1ee38ef9faa9f454bf0f719fc7ed402bc9c44e
Marcel Koch <marcel.koch@kit.edu>
2025-03-20 13:56:33 +0100

    import ginkgo headers for batched solvers

---
+++ include/pdexa-ext/ginkgo/backend/sycl/batch_cg_kernels.hpp
@@ -0,0 +1,73 @@
+// SPDX-FileCopyrightText: 2017 - 2025 The Ginkgo authors
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#pragma once
+
+#include <ginkgo/config.hpp>
+
+#include "pdexa-ext/ginkgo/core/base/batch_struct.hpp"
+
+#if GINKGO_BUILD_SYCL
+
+#include <ginkgo/core/log/batch_logger.hpp>
+
+#include "../../batch_cg_settings.hpp"
+#include "../../batch_criteria.hpp"
+#include "../../batch_identity.hpp"
+#include "../../batch_logger.hpp"
+#include "../../batch_multi_vector.hpp"
+
+
+namespace gko {
+namespace kernels {
+namespace dpcpp {
+namespace batch_template {
+namespace batch_cg {
+
+
+template <typename ValueType, typename Op>
+void apply(
+    std::shared_ptr<const DefaultExecutor> exec,
+    const kernels::batch_cg::settings<remove_complex<ValueType>>& options,
+    const Op* mat, multi_vector_view<const ValueType> b,
+    multi_vector_view<ValueType> x,
+    batch::log::detail::log_data<remove_complex<ValueType>>& logdata)
+    GKO_NOT_IMPLEMENTED;
+
+
+}  // namespace batch_cg
+}  // namespace batch_template
+}  // namespace dpcpp
+}  // namespace kernels
+}  // namespace gko
+
+
+#else
+
+
+namespace gko {
+namespace kernels {
+namespace dpcpp {
+namespace batch_template {
+namespace batch_cg {
+
+
+template <typename ValueType, typename Op>
+void apply(
+    std::shared_ptr<const DefaultExecutor> ,
+    const kernels::batch_cg::settings<remove_complex<ValueType>>& ,
+    const Op , batch::multi_vector::uniform_batch<const ValueType> ,
+    batch::multi_vector::uniform_batch<ValueType> ,
+    batch::log::detail::log_data<remove_complex<ValueType>>&)
+    GKO_NOT_IMPLEMENTED;
+
+
+}  // namespace batch_cg
+}  // namespace batch_template
+}  // namespace dpcpp
+}  // namespace kernels
+}  // namespace gko
+
+
+#endif
---
+++ include/pdexa-ext/ginkgo/core/preconditioner/batch_identity.hpp
@@ -0,0 +1,57 @@
+// SPDX-FileCopyrightText: 2017 - 2025 The Ginkgo authors
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#pragma once
+
+
+#include "pdexa-ext/ginkgo/core/base/batch_struct.hpp"
+
+
+namespace gko::batch_preconditioner {
+
+
+/**
+ * Identity preconditioner for batch solvers. Enables unpreconditioned solves
+ * by performing a copy of the preconditioned vector to the un-preconditioned
+ * vector.
+ */
+template <typename ValueType>
+class Identity final {
+public:
+    using value_type = ValueType;
+
+    /**
+     * The size of the work vector required in case of static allocation.
+     */
+    static constexpr int work_size = 0;
+
+    /**
+     * The size of the work vector required in case of dynamic allocation in
+     * bytes.
+     */
+    static constexpr int dynamic_work_size(int, int) { return 0; }
+
+    /**
+     * Sets the input and generates the identity preconditioner.(Nothing needs
+     * to be actually generated.)
+     */
+    template <typename batch_item_type>
+    constexpr void generate(size_type, const batch_item_type&, ValueType* const)
+    {}
+
+    /**
+     * Applies the preconditioner to the vector. For the identity
+     * preconditioner, this is equivalent to a copy.
+     */
+    constexpr void apply(batch::multi_vector::batch_item<const ValueType> r,
+                         batch::multi_vector::batch_item<ValueType> z) const
+    {
+        for (int i = 0; i < r.num_rows; i++) {
+            z.values[i * z.stride] = r.values[i * r.stride];
+        }
+    }
+};
+
+
+}  // namespace gko::batch_preconditioner
