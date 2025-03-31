d921e5c563dad405c3ac439a9cd3d4b76d93567b
Marcel Koch <marcel.koch@kit.edu>
2025-03-31 15:01:48 +0200

    initialize kokkos

--- app/step-ginkgo/step-ginkgo.cc
+++ app/step-ginkgo/step-ginkgo.cc
@@ -210,6 +210,7 @@ void StepGinkgo<dim>::run() {
 }
 
 int main(int argc, char** argv) {
+  auto g = Kokkos::ScopeGuard(argc, argv);
   auto exec = gko::ext::kokkos::create_executor(memory_space::kokkos_space{});
 
   auto mtx_type = argc >= 2 ? argv[1] : "csr"; {
