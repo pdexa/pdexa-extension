26e6e45cb89bc4544f1eb2bd1e56ee0901924a69
Marcel Koch <marcel.koch@kit.edu>
2025-06-25 12:07:39 +0200

    add spack env to manage deps

--- third-party/gtest.cmake
+++ third-party/gtest.cmake
@@ -1,16 +1,3 @@
 message(STATUS "Enabling package GTest")
 
-if(NOT GTest_FOUND)
-  message(STATUS "Fetching external GTest")
-  fetchcontent_declare(
-          googletest
-          GIT_REPOSITORY https://github.com/google/googletest.git
-          GIT_TAG main
-  )
-
-  set(gtest_force_shared_crt ON CACHE INTERNAL "")
-  set(INSTALL_GTEST OFF CACHE INTERNAL "")
-  fetchcontent_makeavailable(googletest)
-
-  set(GTest_FOUND ON CACHE INTERNAL "")
-endif()
+find_package(GTest REQUIRED)
